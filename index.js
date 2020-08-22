fb2be406a4436803035697dfc55e85c15b965aab
Simon J <jamain.simon@gmail.com>
2020-08-22 21:23:13 +0200

    added methods, to set iteration time and notify totoal players, and maybe some other stuf

--- index.js
+++ index.js
@@ -2,23 +2,56 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+let port = 3000
+let players = []
+let iterationInterval = 3000
+let iterationTimeout = setTimeout(sendIterate, iterationInterval)
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.on('newPlayer', (color) => {
-        io.emit('newPlayer', color);
+
+    socket.on('setIterationInterval', (iterationIntervalReceived) => {
+        iterationInterval = iterationIntervalReceived
     });
+
+    socket.on('stopIterations', () => {
+        clearTimeout(iterationTimeout)
+    });
+
+    socket.on('startIterations', () => {
+        clearTimeout(iterationTimeout)
+        setTimeout(sendIterate, iterationInterval)
+    });
+
+    socket.on('newPlayer', (color) => {
+        players.push(players);
+        io.emit('newPlayer', color);
+        io.emit('allPlayers', players);
+    });
+
+    socket.on('removePlayer', (color) => {
+        var index = array.indexOf(item);
+        if(index != -1){
+            array.splice(index, 1); 
+        }
+        io.emit('allPlayers', players);
+    });
+
     socket.on('newAction', (action) => {
         io.emit('newAction', action);
     });
 });
 
-setInterval(() => {
-    io.emit('iterate');
-}, 1000);
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+function sendIterate(){
+    
+    io.emit('iterate');
+    iterationTimeout = setTimeout(sendIterate, iterationInterval)
+}
+
+http.listen(port, () => {
+  console.log('listening on *:' + port);
 });

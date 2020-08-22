e3fd200d354e2c146f0427cacc318159f050ef2c
Simon J <jamain.simon@gmail.com>
2020-08-22 16:50:15 +0200

    try a fix for cross origin problems

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+io.origins('*:*')
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
